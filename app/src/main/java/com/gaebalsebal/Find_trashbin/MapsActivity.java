570af447ed92db1e905a8c3557419092d3a46070
mobuktodae <87495422+mobuktodae@users.noreply.github.com>
2022-09-03 19:33:46 +0900

    위치 서울로 변경

--- app/src/main/java/com/gaebalsebal/Find_trashbin/MapsActivity.java
+++ app/src/main/java/com/gaebalsebal/Find_trashbin/MapsActivity.java
@@ -30,22 +30,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng latlng = new LatLng(37.55, 126.92);
+        //mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in Korea"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10));
+
+
     }
 }
