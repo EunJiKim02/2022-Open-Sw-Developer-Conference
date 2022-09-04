8e458a5cf9c4da39590fb60f81ee0e12af23119b
mobuktodae <87495422+mobuktodae@users.noreply.github.com>
2022-09-04 16:31:24 +0900

    내 위치 기반 지도 세팅

--- app/src/main/java/com/gaebalsebal/Find_trashbin/MapsActivity.java
+++ app/src/main/java/com/gaebalsebal/Find_trashbin/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.gaebalsebal.Find_trashbin;
 
+
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,18 +16,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.gaebalsebal.Find_trashbin.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double latitude;
+    double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        Intent intent = getIntent();
+        latitude = intent.getDoubleExtra("latitude", 35.8881);
+        longitude = intent.getDoubleExtra("longitude", 128.6112);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this::onMapReady);
 
     }
@@ -29,11 +40,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng latlng = new LatLng(37.55, 126.92);
-        //mMap.addMarker(new MarkerOptions().position(latlng).title("Marker in Korea"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10));
+        LatLng latlng = new LatLng(latitude, longitude);
+        //mMap.addMarker(new MarkerOptions().position(latlng).title("IT 융복합관"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 17));
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
 
     }
+
 }
